6a7ad5be735efd0126492643c8895dddd35cb737
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-08 22:36:00 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost:3306","root","","crud") or die("Connection Failed");
+$conn = mysqli_connect("127.0.0.1","root","","crud") or die("Connection Failed");
 
 ?>
 
