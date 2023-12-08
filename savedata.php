f2513e000bed58bfedcbad40355e4deffb9c2b27
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-08 23:18:44 +0100

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -9,7 +9,7 @@ $conn = mysqli_connect("127.0.0.1","root","","crud") or die("Connection Failed")
 $sql = "INSERT INTO student(sname,saddress,sclass,sphone) VALUES ('{$stu_name}','{$stu_address}','{$stu_class}','{$stu_phone}')";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-header("Location: http://52.146.10.37:80/crud/index.php");
+header("Location: https://crud-appli.azurewebsites.net/index.php");
 
 mysqli_close($conn);
 
