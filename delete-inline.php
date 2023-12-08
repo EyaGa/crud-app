e751e7893febcd9c0da88841a70c76feb1475841
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-08 23:19:57 +0100

    Update delete-inline.php

--- delete-inline.php
+++ delete-inline.php
@@ -7,7 +7,7 @@ include 'config.php';
 $sql = "DELETE FROM student WHERE sid = {$stu_id}";
 $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
 
-header("Location: http://52.146.10.37:80/crud/index.php");
+header("Location: https://crud-appli.azurewebsites.net/index.php");
 
 mysqli_close($conn);
 
