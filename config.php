555cbb00affb12572032e5bcb68a82fd518b1416
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-11 17:43:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,7 @@
 <?php
  error_reporting(E_ALL);
  ini_set('display_errors', 1);
- $host = "localhost";
- $username = "root";
- $password = " "; 
- $database = "crud";
- $conn = mysqli_connect($host, $username, $password, $database);
+ $conn = mysqli_connect('localhost', 'root', '', 'crud');
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 } else {
