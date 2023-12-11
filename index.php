4e986f484b312c0254b009363d269ee3d7c6b03e
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-12 00:05:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'header.php';
     <?php
       error_reporting(E_ALL);
       ini_set('display_errors', 1);
-      $conn = mysqli_connect('localhost', 'root', '1234', 'crud');
+      $conn = mysqli_connect('root@localhost.mysql.database.azure.com', 'root@localhost', '1234', 'crud');
       if (!$conn) {
         die("Connection Failed: " . mysqli_connect_error());
       } else {
