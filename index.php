e2a75d2563399112f0538049b2a2843dee03f247
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-11 20:41:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'header.php';
 <div id="main-content">
     <h2>All Records</h2>
     <?php
-      $conn = mysqli_connect('localhost', 'root', '', 'crud');
+      $conn = mysqli_connect('localhost', 'root@localhost', '', 'crud');
       if (!$conn) {
         die("Connection Failed: " . mysqli_connect_error());
       } else {
