7049220d9f6ad5646f529bb5493d4bedffec8daa
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-12 12:37:25 +0100

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -5,7 +5,7 @@
  $stu_phone = $_POST['sphone'];
  error_reporting(E_ALL);
  ini_set('display_errors', 1);
- $conn = mysqli_connect('127.0.0.1', 'root', 'eYa2096.', 'crud', 3306, '/tmp/mariadb.sock');
+ $conn = mysqli_connect('127.0.0.1', 'root', 'eYa2096.', 'crud', 3307, '/tmp/mariadb.sock');
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 } else {
