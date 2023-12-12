342b4df4a4fc1bd103a486efa33bba02bf361d5a
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-12 10:38:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'header.php';
     <?php
       error_reporting(E_ALL);
       ini_set('display_errors', 1);
-      $conn = mysqli_connect('localhost', 'root', '1234', 'crud', 3307, '/tmp/mariadb.sock');
+      $conn = mysqli_connect('127.0.0.1', 'root', '1234', 'crud', 3307, '/tmp/mariadb.sock');
       if (!$conn) {
         die("Connection Failed: " . mysqli_connect_error());
       } else {
