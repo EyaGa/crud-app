b65bf7aa91d83ad4b255b8bf05eca15a5bdd8387
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-13 18:23:28 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
-  $conn = mysqli_connect('127.0.0.1', 'root', 'eYa2096.', 'crud', 3307, '/tmp/mariadb.sock');
+  $conn = mysqli_connect('127.0.0.1', 'root', 'eYa2096.', 'crud', 3307);
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 } else {
