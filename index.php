e02b0301bad68afab2c633afe58ed3ec3d3812f9
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 22:03:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ include 'header.php';
     <?php
       error_reporting(E_ALL);
       ini_set('display_errors', 1);
-      $conn = new mysqli('', 'root', 'NB*hGJh/bHEdDJwa', 'crud', 3307);
-      if (!$conn) {
-        die("Connection Failed: " . mysqli_connect_error());
+      $conn = new mysqli('192.168.1.15', 'root', 'NB*hGJh/bHEdDJwa', 'crud', 3307);
+      if ($conn->connect_error) {
+        die("Connection Failed: " . $conn->connect_error);
       } else {
        echo "Connected successfully";
       }
