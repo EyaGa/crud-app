bc6a43845933518176f3b0842d6919c3f4efeca9
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 22:22:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,31 @@ include 'header.php';
 <div id="main-content">
     <h2>All Records</h2>
     <?php
-      error_reporting(E_ALL);
-      ini_set('display_errors', 1);
-      $conn = new mysqli('192.168.1.15', 'root', 'NB*hGJh/bHEdDJwa', 'crud', 3307);
-      if ($conn->connect_error) {
-        die("Connection Failed: " . $conn->connect_error);
-      } else {
-       echo "Connected successfully";
-      }
-      $sql = "SELECT * FROM student JOIN studentclass ON student.sclass = studentclass.cid";
-      $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+    $servername = "192.168.1.15"; // Remplacez cette valeur par l'adresse IP réelle du serveur de base de données
+    $username = "root"; // Remplacez cette valeur par le nom d'utilisateur de la base de données
+    $password = "NB*hGJh/bHEdDJwa"; // Remplacez cette valeur par le mot de passe de la base de données
+    // Créer une connexion à la base de données
+    $conn = new mysqli($servername, $username, $password);
 
-      if(mysqli_num_rows($result) > 0)  {
-    ?>
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("Erreur de connexion: " . $conn->connect_error);
+    }
+
+    // Obtenir l'adresse IP du serveur de base de données
+    $sql = "SELECT @@global.hostname, @@global.port FROM dual;";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo "Adresse IP du serveur de base de données: " . $row["@@global.hostname"] . " (port: " . $row["@@global.port"] . ")";
+    } else {
+        echo "Erreur: aucune ligne trouvée";
+    }
+
+    // Fermer la connexion
+    $conn->close();
+?>
     <table cellpadding="7px">
         <thead>
         <th>Id</th>
