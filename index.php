16264394a10dafff40737406a816bad2d3d343e3
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 22:33:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,50 +2,72 @@
 include 'header.php';
 ?>
 <!DOCTYPE html>
-<div id="main-content">
-    <h2>All Records</h2>
-    <?php
-    $servername = "192.168.1.15"; // Remplacez cette valeur par l'adresse IP réelle du serveur de base de données
-    $username = "root"; // Remplacez cette valeur par le nom d'utilisateur de la base de données
-    $password = "NB*hGJh/bHEdDJwa"; // Remplacez cette valeur par le mot de passe de la base de données
-    // Créer une connexion à la base de données
-    $conn = new mysqli($servername, $username, $password);
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Records</title>
+</head>
+<body>
+    <div id="main-content">
+        <h2>All Records</h2>
+        <?php
+        $servername = "192.168.1.15"; // Remplacez cette valeur par l'adresse IP réelle du serveur de base de données
+        $username = "root"; // Remplacez cette valeur par le nom d'utilisateur de la base de données
+        $password = "NB*hGJh/bHEdDJwa"; // Remplacez cette valeur par le mot de passe de la base de données
+        $database = "crud"; // Remplacez cette valeur par le nom de votre base de données
+        $port = 3307; // Remplacez cette valeur par le port correct de votre base de données
 
-?>
-    <table cellpadding="7px">
-        <thead>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Class</th>
-        <th>Phone</th>
-        <th>Action</th>
-        </thead>
-        <tbody>
-          <?php
-            while($row = mysqli_fetch_assoc($result)){
-             echo "<tr>
-             <td>{$row['sid']}</td>
-             <td>{$row['sname']}</td>
-             <td>{$row['saddress']}</td>
-             <td>{$row['cname']}</td>
-             <td>{$row['sphone']}</td>
-             <td> 
-             <a href='edit.php?id={$row['sid']}'>Edit</a> 
-             <a href='delete-inline.php?id={$row['sid']}'>Delete</a>
-             </td>
-            </tr>";
-          }
-          ?>
-        </tbody>
-    </table>
-    <?php 
-  } else {
-    echo "<h2>No Record Found</h2>";
-  }
-  mysqli_close($conn);
-  ?>
-</div>
-</div>
+        // Créer une connexion à la base de données
+        $conn = new mysqli($servername, $username, $password, $database, $port);
+
+        // Vérifier la connexion
+        if ($conn->connect_error) {
+            die("Erreur de connexion: " . $conn->connect_error);
+        }
+
+        // Exécutez la requête SQL pour récupérer les données des étudiants
+        $sql = "SELECT * FROM student JOIN studentclass ON student.sclass = studentclass.cid";
+        $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+
+        // Afficher les résultats dans un tableau
+        if (mysqli_num_rows($result) > 0) {
+        ?>
+            <table cellpadding="7px">
+                <thead>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Class</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>
+                        <td>{$row['sid']}</td>
+                        <td>{$row['sname']}</td>
+                        <td>{$row['saddress']}</td>
+                        <td>{$row['cname']}</td>
+                        <td>{$row['sphone']}</td>
+                        <td> 
+                        <a href='edit.php?id={$row['sid']}'>Edit</a> 
+                        <a href='delete-inline.php?id={$row['sid']}'>Delete</a>
+                        </td>
+                        </tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        <?php
+        } else {
+            echo "<h2>No Record Found</h2>";
+        }
+
+        // Fermer la connexion
+        mysqli_close($conn);
+        ?>
+    </div>
 </body>
 </html>
