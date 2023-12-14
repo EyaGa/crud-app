f7d001d06da1c25392653f628fef5c261cdd85aa
Eya Gaied <98452533+EyaGa@users.noreply.github.com>
2023-12-14 15:54:35 +0100

    Update savedata.php

--- savedata.php
+++ savedata.php
@@ -5,7 +5,7 @@
  $stu_phone = $_POST['sphone'];
  error_reporting(E_ALL);
  ini_set('display_errors', 1);
- $conn = mysqli_connect('127.0.0.1', 'nadia', 'eYa2096.', 'crud', 3307);
+ $conn = mysqli_connect('127.0.0.1', 'root', '', 'crud', 3307);
 if (!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 } else {
